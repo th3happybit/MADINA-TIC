c17139caaab5c8cfcbfb53509b628dc446271a29
Oussama Messabih <oussamamessabih02@gmail.com>
2020-05-21 00:38:20 +0000

    change deployment scripts

--- deploy_front.sh
+++ deploy_front.sh
@@ -3,7 +3,6 @@ pwd
 git pull
 cd ..
 pwd
-find ./ -type f -exec sed -i 's/157.230.19.233/157.230.19.233/g' {} +
 sudo docker build -t madina-tic/frontend:0.2 ./MADINA-TIC-FRONT
 sudo docker-compose down
 sudo docker volume rm madina-tic_MADINA-TIC-FRONT
